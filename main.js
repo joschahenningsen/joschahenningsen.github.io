62c3728732ec19f91132ea43515f81cce3f7b3fb
Joscha Henningsen <joscha.henningsen@cs.tum.edu>
2019-12-04 15:36:03 +0100

    Code cleanup

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var myTerminal = new Terminal();
-var indent = "<span class='indent'></span>"
+let myTerminal = new Terminal();
+let indent = "<span class='indent'></span>";
 
 window.onload = function() {
     document.getElementById("terminal").append(myTerminal.html);
     setup();
     readMessage();
-}
+};
 
 function setup(){
     myTerminal.print("Hi,");
     myTerminal.print("I'm glad you made it this far!");
     myTerminal.print("Feel free to explore");
-    myTerminal.print("(C) 🌈 Joscha Henningsen 2019");
+    myTerminal.print("(C) Joscha Henningsen 2019");
     myTerminal.print("\u2063");
 }
 
@@ -20,9 +20,29 @@ function readMessage(){
 }
 
 function messageReceived(message){
-    printlines(getResponse(message));
+    printLines(getResponse(message));
     readMessage();
 }
+
+
+//Don't want plaintext email addresses online
+let caesarShift = function(str, amount) {
+	if (amount < 0)
+		return caesarShift(str, amount + 26);
+    let output = '';
+    for (let i = 0; i < str.length; i ++) {
+        let c = str[i];
+        if (c.match(/[a-z]/i)) {
+            let code = str.charCodeAt(i);
+            if ((code >= 65) && (code <= 90))
+				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+			else if ((code >= 97) && (code <= 122))
+				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+		}
+		output += c;
+	}
+	return output;
+};
 
 function getResponse(input){
     switch(input){
@@ -31,7 +51,7 @@ function getResponse(input){
 	    case "./snake":
 	        return "try again later";
         case "help":
-            var res = "Available commands:\n";
+            let res = "Available commands:\n";
             res += indent+"help\n";
             res += indent+"whoami\n";
             res += indent+"contact\n";
@@ -40,7 +60,7 @@ function getResponse(input){
             res += indent+"exit\n";
             return res;
         case "whoami":
-            return '{\n<span class="indent"></span><span class="highlight2">"name"</span>: <span class="highlight1">"Joscha Henningsen"</span>,\n<span class="indent"></span><span class="highlight2">"birthdate"</span>: <span class="highlight1">"23.09.1998"</span>,\n<span class="indent"></span><span class="highlight2">"placeOfBirth"</span>: <span class="highlight1">"Hamburg, Germany"</span>,\n<span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich, Germany"</span>,\n<span class="indent"></span>\n<span class="indent"></span><span class="highlight2">"education:"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"grundschuleGenslerstraße"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Primary School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2004"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2008"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"albertSchweitzerGymnasium"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Middle School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2008"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2015"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"gymnasiumCorvestraße"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"High School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2015"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2017"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"TUM"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Bachelor Studies, Informatics"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2018"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2021(expected)"</span>\n<span class="indent"></span><span class="indent"></span>}\n<span class="indent"></span>},\n<span class="indent"></span>\n<span class="indent"></span><span class="highlight2">"work"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"TUM"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Tutor: Praktikum Grundlagen der Programmierung"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"period"</span>: <span class="highlight1">"Winter Semester 2019/20"</span>\n<span class="indent"></span><span class="indent"></span>}\n<span class="indent"></span>}\n}'
+            return '{\n<span class="indent"></span><span class="highlight2">"name"</span>: <span class="highlight1">"Joscha Henningsen"</span>,\n<span class="indent"></span><span class="highlight2">"birthdate"</span>: <span class="highlight1">"23.09.1998"</span>,\n<span class="indent"></span><span class="highlight2">"placeOfBirth"</span>: <span class="highlight1">"Hamburg, Germany"</span>,\n<span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich, Germany"</span>,\n<span class="indent"></span>\n<span class="indent"></span><span class="highlight2">"education:"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"grundschuleGenslerstraße"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Primary School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2004"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2008"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"albertSchweitzerGymnasium"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Middle School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2008"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2015"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"gymnasiumCorvestraße"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"High School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2015"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2017"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"TUM"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Bachelor Studies, Informatics"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2018"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2021(expected)"</span>\n<span class="indent"></span><span class="indent"></span>}\n<span class="indent"></span>},\n<span class="indent"></span>\n<span class="indent"></span><span class="highlight2">"work"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"TUM"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Tutor: Praktikum Grundlagen der Programmierung"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"period"</span>: <span class="highlight1">"Winter Semester 2019/20"</span>\n<span class="indent"></span><span class="indent"></span>}\n<span class="indent"></span>}\n}';
         case "clear":
             myTerminal.clear();
             return "";
@@ -52,23 +72,23 @@ function getResponse(input){
             window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
             return "";
         case "contact":
-            var mail = caesarShift("ij@kptdibt.qbhf", -1);
-            var ret = "Uhh, you want to get in touch? Awesome, I'm always up for a chat:<br><br>";
+            let mail = caesarShift("ij@kptdibt.qbhf", -1);
+            let ret = "Uhh, you want to get in touch? Awesome, I'm always up for a chat:<br><br>";
             ret += indent+"<a style='color:#fff;' href='mailto:"+mail+"'>"+mail+"</a> &lt-- click here<br>";
             ret += indent+"or here! --> <a style='color:#fff;' href='https://twitter.com/joschahen'>twitter.com/joschahen</a><br>";
             ret += indent+"<a style='color:#fff;' href='https://t.me/joschahenningsen'>t.me/joschahenningsen</a> <-- maby even there<br><br>";
-            return ret
+            return ret;
         default:
             return "<span class='error'>✘</span> command not found: "+input;
     }
 }
 
-async function printlines(lines){
+async function printLines(lines){
     if(lines === ""){
         return;
     }
-    var list = lines.split("\n");
-    for(i = 0; i < list.length; i++){
+    let list = lines.split("\n");
+    for(let i = 0; i < list.length; i++){
         myTerminal.print(list[i]);
     }
 }
@@ -77,22 +97,3 @@ async function printlines(lines){
 function Sleep(milliseconds) {
    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
-
-//Don't want plaintext email addresses online
-var caesarShift = function(str, amount) {
-	if (amount < 0)
-		return caesarShift(str, amount + 26);
-	var output = '';
-	for (var i = 0; i < str.length; i ++) {
-		var c = str[i];
-		if (c.match(/[a-z]/i)) {
-			var code = str.charCodeAt(i);
-			if ((code >= 65) && (code <= 90))
-				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-			else if ((code >= 97) && (code <= 122))
-				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
-		}
-		output += c;
-	}
-	return output;
-};
--- terminal.js
+++ terminal.js
@@ -1,188 +1,190 @@
-var Terminal = (function () {
-	// PROMPT_TYPE
-	var PROMPT_INPUT = 1, PROMPT_PASSWORD = 2, PROMPT_CONFIRM = 3
-	var inputPrompt = "<span class='error'>❤ </span> guest@joschas.page"
-	var workingDir = "~"
-    var inputPromptShape = ""
-	var inputPromptHTML = "<span id='inputPromptUser'>"+inputPrompt+"</span>"+"<span id='inputPromptShapeUser'>"+inputPromptShape+"</span>"
-	inputPromptHTML += "<span id='inputPromptLocation'>"+workingDir+"</span>"+"<span id='inputPromptShapeLocation'>"+inputPromptShape+"</span>"
-    
-	var fireCursorInterval = function (inputField, terminalObj) {
-		var cursor = terminalObj._cursor
-		setTimeout(function () {
-			if (inputField.parentElement && terminalObj._shouldBlinkCursor) {
-				cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden' : 'visible'
-				fireCursorInterval(inputField, terminalObj)
-			} else {
-				cursor.style.visibility = 'visible'
-			}
-		}, 500)
-	}
+let Terminal = (function () {
+    // PROMPT_TYPE
+    let PROMPT_INPUT = 1, PROMPT_PASSWORD = 2, PROMPT_CONFIRM = 3;
+    let inputPrompt = "<span class='error' style='font-size:.7em;'>\u26a1 </span> guest@joschas.page";
+    let workingDir = "~";
+    let inputPromptShape = "";
+    let inputPromptHTML = "<span id='inputPromptUser'>" + inputPrompt + "</span>" + "<span id='inputPromptShapeUser'>" + inputPromptShape + "</span>";
+    inputPromptHTML += "<span id='inputPromptLocation'>" + workingDir + "</span>" + "<span id='inputPromptShapeLocation'>" + inputPromptShape + "</span>";
 
-	var firstPrompt = true;
-	promptInput = function (terminalObj, message, PROMPT_TYPE, callback) {
-		var shouldDisplayInput = (PROMPT_TYPE === PROMPT_INPUT)
-		var inputField = document.createElement('input')
+    let fireCursorInterval = function (inputField, terminalObj) {
+        let cursor = terminalObj._cursor;
+        setTimeout(function () {
+            if (inputField.parentElement && terminalObj._shouldBlinkCursor) {
+                cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden' : 'visible';
+                fireCursorInterval(inputField, terminalObj)
+            } else {
+                cursor.style.visibility = 'visible'
+            }
+        }, 500)
+    };
 
-		inputField.style.position = 'absolute'
-		inputField.style.zIndex = '-100'
-		inputField.style.outline = 'none'
-		inputField.style.border = 'none'
-		inputField.style.opacity = '0'
-		inputField.style.fontSize = '0.2em'
+    let firstPrompt = true;
+    let promptInput = function (terminalObj, message, PROMPT_TYPE, callback) {
+        let shouldDisplayInput = (PROMPT_TYPE === PROMPT_INPUT);
+        let inputField = document.createElement('input');
 
-		terminalObj._inputLine.textContent = ''
-        terminalObj._inputPrompt.innerHTML = inputPromptHTML
-		terminalObj._input.style.display = 'block'
-		terminalObj.html.appendChild(inputField)
-		fireCursorInterval(inputField, terminalObj)
+        inputField.style.position = 'absolute';
+        inputField.style.zIndex = '-100';
+        inputField.style.outline = 'none';
+        inputField.style.border = 'none';
+        inputField.style.opacity = '0';
+        inputField.style.fontSize = '0.2em';
 
-		if (message.length) terminalObj.print(PROMPT_TYPE === PROMPT_CONFIRM ? message + ' (y/n)' : message)
+        terminalObj._inputLine.textContent = '';
+        terminalObj._inputPrompt.innerHTML = inputPromptHTML;
+        terminalObj._input.style.display = 'block';
+        terminalObj.html.appendChild(inputField);
+        fireCursorInterval(inputField, terminalObj);
 
-		inputField.onblur = function () {
-			terminalObj._cursor.style.display = 'none'
-		}
+        if (message.length) terminalObj.print(PROMPT_TYPE === PROMPT_CONFIRM ? message + ' (y/n)' : message);
 
-		inputField.onfocus = function () {
-			inputField.value = terminalObj._inputLine.textContent.toLowerCase();
-			terminalObj._cursor.style.display = 'inline'
-		}
+        inputField.onblur = function () {
+            terminalObj._cursor.style.display = 'none'
+        };
 
-		terminalObj.html.onclick = function () {
-			inputField.focus()
-		}
+        inputField.onfocus = function () {
+            inputField.value = terminalObj._inputLine.textContent.toLowerCase();
+            terminalObj._cursor.style.display = 'inline'
+        };
 
-		inputField.onkeydown = function (e) {
-			if (e.which === 37 || e.which === 39 || e.which === 38 || e.which === 40 || e.which === 9) {
-				e.preventDefault()
-			} else if (shouldDisplayInput && e.which !== 13) {
-				setTimeout(function () {
-                    terminalObj._inputPrompt.innterHTML = inputPromptHTML
-					terminalObj._inputLine.textContent = inputField.value.toLowerCase()
-				}, 1)
-			}
-		}
-		
-		inputField.onkeyup = function (e) {
-			if (PROMPT_TYPE === PROMPT_CONFIRM || e.which === 13) {
-				terminalObj._input.style.display = 'none'
-				var inputValue = inputField.value.toLowerCase()
-				if (shouldDisplayInput) terminalObj.printUser(inputValue)
-				terminalObj.html.removeChild(inputField)
-				if (typeof(callback) === 'function') {
-					if (PROMPT_TYPE === PROMPT_CONFIRM) {
-						callback(inputValue.toUpperCase()[0] === 'Y' ? true : false)
-					} else callback(inputValue)
-				}
-			}
-		}
-		if (firstPrompt) {
-			firstPrompt = false
-			setTimeout(function () { inputField.focus()	}, 50)
-		} else {
-			inputField.focus()
-		}
-	}
+        terminalObj.html.onclick = function () {
+            inputField.focus()
+        };
 
-	var terminalBeep
+        inputField.onkeydown = function (e) {
+            if (e.which === 37 || e.which === 39 || e.which === 38 || e.which === 40 || e.which === 9) {
+                e.preventDefault()
+            } else if (shouldDisplayInput && e.which !== 13) {
+                setTimeout(function () {
+                    terminalObj._inputPrompt.innterHTML = inputPromptHTML;
+                    terminalObj._inputLine.textContent = inputField.value.toLowerCase()
+                }, 1)
+            }
+        };
 
-	var TerminalConstructor = function (id) {
-		this.html = document.createElement('div')
-		this.html.className = 'Terminal'
-		if (typeof(id) === 'string') { this.html.id = id }
+        inputField.onkeyup = function (e) {
+            if (PROMPT_TYPE === PROMPT_CONFIRM || e.which === 13) {
+                terminalObj._input.style.display = 'none';
+                var inputValue = inputField.value.toLowerCase();
+                if (shouldDisplayInput) terminalObj.printUser(inputValue);
+                terminalObj.html.removeChild(inputField);
+                if (typeof (callback) === 'function') {
+                    if (PROMPT_TYPE === PROMPT_CONFIRM) {
+                        callback(inputValue.toUpperCase()[0] === 'Y')
+                    } else callback(inputValue)
+                }
+            }
+        };
+        if (firstPrompt) {
+            firstPrompt = false;
+            setTimeout(function () {
+                inputField.focus()
+            }, 50)
+        } else {
+            inputField.focus()
+        }
+    };
 
-		this._innerWindow = document.createElement('div')
-		this._output = document.createElement('p')
-		this._inputPrompt = document.createElement('span');
-		this._inputPrompt.innterHTML = inputPromptHTML;
-		this._inputLine = document.createElement('span') //the span element where the users input is put
-		this._cursor = document.createElement('span')
-		this._input = document.createElement('p') //the full element administering the user input, including cursor
+    let TerminalConstructor = function (id) {
+        this.html = document.createElement('div');
+        this.html.className = 'Terminal';
+        if (typeof (id) === 'string') {
+            this.html.id = id
+        }
 
-		this._shouldBlinkCursor = true
+        this._innerWindow = document.createElement('div');
+        this._output = document.createElement('p');
+        this._inputPrompt = document.createElement('span');
+        this._inputPrompt.innterHTML = inputPromptHTML;
+        this._inputLine = document.createElement('span'); //the span element where the users input is put
+        this._cursor = document.createElement('span');
+        this._input = document.createElement('p'); //the full element administering the user input, including cursor
 
-		this.print = function (message) {
-			var newLine = document.createElement('div')
-			newLine.innerHTML = message
-			this._output.appendChild(newLine)
-		}
-		
-		this.printUser = function (message) {
-			var newLine = document.createElement('div')
-			newLine.innerHTML = inputPromptHTML+message
-			this._output.appendChild(newLine)
-		}
+        this._shouldBlinkCursor = true;
 
-		this.input = function (message, callback) {
-			promptInput(this, message, PROMPT_INPUT, callback)
-		}
+        this.print = function (message) {
+            var newLine = document.createElement('div');
+            newLine.innerHTML = message;
+            this._output.appendChild(newLine)
+        };
 
-		this.password = function (message, callback) {
-			promptInput(this, message, PROMPT_PASSWORD, callback)
-		}
+        this.printUser = function (message) {
+            var newLine = document.createElement('div');
+            newLine.innerHTML = inputPromptHTML + message;
+            this._output.appendChild(newLine)
+        };
 
-		this.confirm = function (message, callback) {
-			promptInput(this, message, PROMPT_CONFIRM, callback)
-		}
+        this.input = function (message, callback) {
+            promptInput(this, message, PROMPT_INPUT, callback)
+        };
 
-		this.clear = function () {
-			this._output.innerHTML = ''
-		}
+        this.password = function (message, callback) {
+            promptInput(this, message, PROMPT_PASSWORD, callback)
+        };
 
-		this.sleep = function (milliseconds, callback) {
-			setTimeout(callback, milliseconds)
-		}
+        this.confirm = function (message, callback) {
+            promptInput(this, message, PROMPT_CONFIRM, callback)
+        };
 
-		this.setTextSize = function (size) {
-			this._output.style.fontSize = size
-			this._input.style.fontSize = size
-		}
+        this.clear = function () {
+            this._output.innerHTML = ''
+        };
 
-		this.setTextColor = function (col) {
-			this.html.style.color = col
-			this._cursor.style.background = col
-		}
+        this.sleep = function (milliseconds, callback) {
+            setTimeout(callback, milliseconds)
+        };
 
-		this.setBackgroundColor = function (col) {
-			this.html.style.background = col
-		}
+        this.setTextSize = function (size) {
+            this._output.style.fontSize = size;
+            this._input.style.fontSize = size
+        };
 
-		this.setWidth = function (width) {
-			this.html.style.width = width
-		}
+        this.setTextColor = function (col) {
+            this.html.style.color = col;
+            this._cursor.style.background = col
+        };
 
-		this.setHeight = function (height) {
-			this.html.style.height = height
-		}
+        this.setBackgroundColor = function (col) {
+            this.html.style.background = col
+        };
 
-		this.blinkingCursor = function (bool) {
-			bool = bool.toString().toUpperCase()
-			this._shouldBlinkCursor = (bool === 'TRUE' || bool === '1' || bool === 'YES')
-		}
+        this.setWidth = function (width) {
+            this.html.style.width = width
+        };
 
-		this._input.appendChild(this._inputPrompt)
-		this._input.appendChild(this._inputLine)
-		this._input.appendChild(this._cursor)
-		this._innerWindow.appendChild(this._output)
-		this._innerWindow.appendChild(this._input)
-		this.html.appendChild(this._innerWindow)
+        this.setHeight = function (height) {
+            this.html.style.height = height
+        };
 
-		this.setBackgroundColor('black')
-		this.setTextColor('white')
-		this.setTextSize('1em')
-		this.setWidth('100%')
-		this.setHeight('100%')
+        this.blinkingCursor = function (bool) {
+            bool = bool.toString().toUpperCase();
+            this._shouldBlinkCursor = (bool === 'TRUE' || bool === '1' || bool === 'YES')
+        };
 
-		this.html.style.fontFamily = 'Monaco, Courier'
-		this.html.style.margin = '0'
-		this._innerWindow.style.padding = '10px'
-		this._input.style.margin = '0'
-		this._output.style.margin = '0'
-		this._cursor.style.background = 'white'
-		this._cursor.innerHTML = 'C' //put something in the cursor..
-		this._cursor.style.display = 'none' //then hide it
-		this._input.style.display = 'none'
-	}
+        this._input.appendChild(this._inputPrompt);
+        this._input.appendChild(this._inputLine);
+        this._input.appendChild(this._cursor);
+        this._innerWindow.appendChild(this._output);
+        this._innerWindow.appendChild(this._input);
+        this.html.appendChild(this._innerWindow);
 
-	return TerminalConstructor
-}())
+        this.setBackgroundColor('black');
+        this.setTextColor('white');
+        this.setTextSize('1em');
+        this.setWidth('100%');
+        this.setHeight('100%');
+
+        this.html.style.fontFamily = 'Monaco, Courier';
+        this.html.style.margin = '0';
+        this._innerWindow.style.padding = '10px';
+        this._input.style.margin = '0';
+        this._output.style.margin = '0';
+        this._cursor.style.background = 'white';
+        this._cursor.innerHTML = 'C'; //put something in the cursor..
+        this._cursor.style.display = 'none'; //then hide it
+        this._input.style.display = 'none'
+    };
+
+    return TerminalConstructor
+}());
