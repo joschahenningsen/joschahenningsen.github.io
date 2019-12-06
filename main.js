872143e0a700f9dbaa32932c67217af3d1b8b55f
Joscha Henningsen <joscha.henningsen@cs.tum.edu>
2019-12-06 20:46:48 +0100

    make it rain

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
 let myTerminal = new Terminal();
-let indent = "<span class='indent'></span>";
+const indent = "<span class='indent'></span>";
+const whoamiObj= {"name": "Joscha Henningsen",
+    "birthday": "23.09.1998",
+    "placeOfBirth": "Hamburg, Germany",
+    "location": "Munich, Germany",
+    "education:": {
+        "albertSchweitzerGymnasium": {
+            "type": "Middle School",
+            "location": "Hamburg",
+            "from": "2008",
+            "to": "2015"
+        },
+        "gymnasiumCorvestraße": {
+            "type": "High School",
+            "location": "Hamburg",
+            "from": "2015",
+            "to": "2017"
+        },
+        "TUM": {
+            "type": "Bachelor Studies, Informatics",
+            "location": "Munich",
+            "from": "2018",
+            "to": "2021(expected)"
+        }
+    },
+    "work": {
+        "TUM": {
+            "type" : "teaching",
+            "title": "Tutor: Praktikum Grundlagen der Programmierung",
+            "location": "Munich",
+            "period": "Winter Semester 2019/20"
+        }
+    }
+};
+const whoamiStr = JSON.stringify(whoamiObj, undefined, 2);
 
 window.onload = function() {
     document.getElementById("terminal").append(myTerminal.html);
-    setup();
-    readMessage();
-};
-
-function setup(){
     myTerminal.print("Hi,");
     myTerminal.print("I'm glad you made it this far!");
     myTerminal.print("Feel free to explore");
-    myTerminal.print("(C) Joscha Henningsen 2019");
+    myTerminal.print("(C) Joscha Henningsen 2019 🌈");
     myTerminal.print("\u2063");
-}
+    readMessage();
+};
 
 function readMessage(){
     myTerminal.input("", function (userInput) {messageReceived(userInput)});
@@ -60,6 +90,7 @@ function getResponse(input){
             res += indent+"exit\n";
             return res;
         case "whoami":
+            //return "<pre>\n" + syntaxHighlight(whoamiStr) + "</pre>\n";
             return '{\n<span class="indent"></span><span class="highlight2">"name"</span>: <span class="highlight1">"Joscha Henningsen"</span>,\n<span class="indent"></span><span class="highlight2">"birthday"</span>: <span class="highlight1">"23.09.1998"</span>,\n<span class="indent"></span><span class="highlight2">"placeOfBirth"</span>: <span class="highlight1">"Hamburg, Germany"</span>,\n<span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich, Germany"</span>,\n<span class="indent"></span>\n<span class="indent"></span><span class="highlight2">"education:"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"grundschuleGenslerstraße"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Primary School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2004"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2008"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"albertSchweitzerGymnasium"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Middle School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2008"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2015"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"gymnasiumCorvestraße"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"High School"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Hamburg"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2015"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2017"</span>\n<span class="indent"></span><span class="indent"></span>},\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"TUM"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Bachelor Studies, Informatics"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"from"</span>: <span class="highlight1">"2018"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"to"</span>: <span class="highlight1">"2021(expected)"</span>\n<span class="indent"></span><span class="indent"></span>}\n<span class="indent"></span>},\n<span class="indent"></span>\n<span class="indent"></span><span class="highlight2">"work"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="highlight2">"TUM"</span>: {\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"type"</span>: <span class="highlight1">"Tutor: Praktikum Grundlagen der Programmierung"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"location"</span>: <span class="highlight1">"Munich"</span>,\n<span class="indent"></span><span class="indent"></span><span class="indent"></span><span class="highlight2">"period"</span>: <span class="highlight1">"Winter Semester 2019/20"</span>\n<span class="indent"></span><span class="indent"></span>}\n<span class="indent"></span>}\n}';
         case "clear":
             myTerminal.clear();
@@ -75,9 +106,16 @@ function getResponse(input){
             let mail = caesarShift("ij@kptdibt.qbhf", -1);
             let ret = "Uhh, you want to get in touch? Awesome, I'm always up for a chat:<br><br>";
             ret += indent+"<a style='color:#fff;' href='mailto:"+mail+"'>"+mail+"</a> &lt-- click here<br>";
-            ret += indent+"or here! --> <a style='color:#fff;' href='https://twitter.com/joschahen'>twitter.com/joschahen</a><br>";
-            ret += indent+"<a style='color:#fff;' href='https://t.me/joschahenningsen'>t.me/joschahenningsen</a> <-- maybe even there<br><br>";
+            ret += indent+"or here! --&gt <a style='color:#fff;' href='https://twitter.com/joschahen'>twitter.com/joschahen</a><br>";
+            ret += indent+"<a style='color:#fff;' href='https://t.me/joschahenningsen'>t.me/joschahenningsen</a> &lt-- maybe even there<br><br>";
             return ret;
+        case "./rain.out":
+            myTerminal.clear();
+            const rainP5 = new p5(rainSketch);
+            if(document.getElementById("defaultCanvas0")!==null){
+                document.getElementById("defaultCanvas0").style.visibility = "visible";
+            }
+            return "";
         default:
             return "<span class='error'>✘</span> command not found: "+input;
     }
@@ -93,7 +131,25 @@ async function printLines(lines){
     }
 }
 
-
 function Sleep(milliseconds) {
    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+function syntaxHighlight(jsonStr) {
+    jsonStr = jsonStr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return jsonStr.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        let cls = '';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'highlight1';
+            } else {
+                cls = 'highlight2';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '"> ' + match + '</span>';
+    });
 }
---
+++ rain.js
@@ -0,0 +1,53 @@
+let rainSketch = function(p){
+    p.raindrops = [];
+    p.font;
+    
+    p.preload = function() {
+        p.font = p.loadFont('https://joschahenningsen.github.io/RobotoMonoforPowerline.ttf');
+    }
+
+    p.setup = function() {
+        p.createCanvas(p.displayWidth, p.displayHeight);
+        for (let i = 0; i < 400; i++) {
+            p.raindrops[p.raindrops.length]={
+                x:p.random(0, p.displayWidth),
+                y:p.random(-p.displayHeight,p.displayHeight),
+                z:p.random(0,5)
+            }
+        }
+        p.textSize(16);
+        p.textAlign(p.CENTER, p.CENTER);
+        p.textFont(p.font);
+        p.frameRate(120);
+        p.fullscreen();
+    }
+
+    p.draw = function(){
+        // draw score text
+        p.background("#0f111a");
+        p.fill(255);
+        p.text("quit: q/click anywhere", 120, 20);
+        p.stroke(255);
+        p.raindrops.forEach(element => {
+            p.line(element.x, element.y, element.x+element.z/2, element.y+element.z*1.5);
+            element.y+=5+element.z;
+            element.x+=element.z/3;
+            if(element.y>p.displayHeight){
+                element.x = p.random(-50, p.displayWidth);
+                element.y = 0;
+            }
+        });
+    }
+
+    p.keyPressed = function() {
+        if (p.keyCode === 81) {
+            document.getElementById("defaultCanvas0").style.visibility = "hidden";
+            p.stop();
+        }
+    }
+
+    p.mouseClicked = function() {
+        document.getElementById("defaultCanvas0").style.visibility = "hidden";
+        p.stop();
+    }
+}
