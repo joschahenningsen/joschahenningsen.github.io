11d95296ddba6e3654eca41afb681a4faf404b53
Joscha Henningsen <joscha.henningsen@cs.tum.edu>
2020-01-12 14:40:52 +0100

    Modified installation script

--- backend/installDependencies.sh
+++ backend/installDependencies.sh
@@ -2,6 +2,8 @@
 sudo apt update 
 sudo apt upgrade -y
 sudo apt install fortune cowsay
+echo "<?php" > commandLocations.php
+echo "" >> commandLocations.php
 echo "\$cowsay = \"$(which cowsay)\";" >> commandLocations.php
 echo "\$fortune = \"$(which fortune)\";" >> commandLocations.php
 echo ":)"
